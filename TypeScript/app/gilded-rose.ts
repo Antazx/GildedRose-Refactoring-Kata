af351f2cd0f2ed909bc7cad7407c84063dccadaf
Guillermo Anta <guillermo.anta@telecomboutique.com>
2023-06-12 13:34:15 +0200

    Remove magic values and add new requirement

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -11,6 +11,21 @@ export class Item {
 }
 
 export class GildedRose {
+  private AGED_BRIE = 'Aged Brie';
+  private AGED_BRIE_DECREMENT_ON_SELL_LIMIT_DAYS = 0;
+
+  private TAFKAL80ETC_BACKSTAGE_PASS = 'Backstage passes to a TAFKAL80ETC concert';
+  private TAFKAL80ETC_BACKSTAGE_PASS_X2_INCREMENT_ON_SELL_LIMIT_DAYS = 11;
+  private TAFKAL80ETC_BACKSTAGE_PASS_X3_INCREMENT_ON_SELL_LIMIT_DAYS = 6;
+  private TAFKAL80ETC_BACKSTAGE_PASS_AFTER_SELL_LIMIT_DAYS = 0;
+
+  private SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
+
+  private MAX_QUALITY = 50;
+  private MIN_QUALITY = 0;
+  private MIN_CONJURED_QUALITY = 2;
+  private DEFAULT_X2_DECREMENT_ON_SELL_LIMIT_DAYS = 0;
+
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
@@ -18,52 +33,79 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+    for (const item of this.items) {
+      if (item.name === this.AGED_BRIE) {
+        this.decrementSellIn(item);
+        this.updateAgedBrieQuality(item);
+        continue;
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+      if (item.name === this.TAFKAL80ETC_BACKSTAGE_PASS) {
+        this.decrementSellIn(item);
+        this.updateBackstagePassQuality(item);
+        continue;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+
+      if (item.name === this.SULFURAS_HAND_OF_RAGNAROS) continue;
+
+      this.decrementSellIn(item);
+      if (this.isConjured(item)) this.updateDefaultItemQuality(item);
+      if (!this.isConjured(item)) this.updateConjuredItemQuality(item);
     }
 
     return this.items;
+  }
+
+  private isConjured(item) {
+    return true;
+  }
+
+  private decrementSellIn(item: Item) {
+    item.sellIn = item.sellIn - 1;
+  }
+
+  private updateAgedBrieQuality(item: Item) {
+    if (item.quality >= this.MAX_QUALITY) return;
+    const { quality } = item;
+    item.quality =
+      item.sellIn < this.AGED_BRIE_DECREMENT_ON_SELL_LIMIT_DAYS ? quality + 2 : quality + 1;
+  }
+
+  private updateBackstagePassQuality(item: Item) {
+    if (item.quality >= this.MAX_QUALITY) return;
+    let { quality } = item;
+
+    quality++;
+    if (item.sellIn < this.TAFKAL80ETC_BACKSTAGE_PASS_X2_INCREMENT_ON_SELL_LIMIT_DAYS) quality++;
+    if (item.sellIn < this.TAFKAL80ETC_BACKSTAGE_PASS_X3_INCREMENT_ON_SELL_LIMIT_DAYS) quality++;
+
+    item.quality =
+      item.sellIn < this.TAFKAL80ETC_BACKSTAGE_PASS_AFTER_SELL_LIMIT_DAYS ? 0 : quality;
+  }
+  private updateDefaultItemQuality(item: Item) {
+    if (item.quality <= this.MIN_QUALITY) return;
+
+    let { quality } = item;
+
+    quality--;
+    if (item.sellIn < this.DEFAULT_X2_DECREMENT_ON_SELL_LIMIT_DAYS && quality > this.MIN_QUALITY)
+      quality--;
+
+    item.quality = quality;
+  }
+
+  private updateConjuredItemQuality(item: Item) {
+    if (item.quality <= this.MIN_CONJURED_QUALITY) return;
+
+    let { quality } = item;
+
+    quality = quality - 2;
+    if (
+      item.sellIn < this.DEFAULT_X2_DECREMENT_ON_SELL_LIMIT_DAYS &&
+      quality > this.MIN_CONJURED_QUALITY
+    )
+      quality = quality - 2;
+
+    item.quality = quality;
   }
 }
